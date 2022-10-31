2cb8e33c3a6d5c69c2467397edb3c664cbc22181
oscurart <info@oscurart.com.ar>
2022-10-31 08:57:54 -0300

    add curves to mesh

---
+++ oscurart_curves_to_mesh.py
@@ -0,0 +1,26 @@
+import bpy
+
+vi = 0
+edgelist = []
+vertlist = []
+
+for stroke in bpy.context.object.data.curves:
+
+    
+    for p in stroke.points:
+        vertlist.append(p.position) 
+    
+    cantPuntos= len(stroke.points)-1    
+    
+    for a in range(0,cantPuntos):
+        edgelist.append((vi,vi+1))
+        vi = vi+1
+
+    vi += 1 
+
+me = bpy.data.meshes.new("data")
+gpdb = bpy.data.objects.new("Annotation", me)
+me.from_pydata(vertlist,edgelist,[])
+me.update()
+bpy.context.scene.collection.objects.link(gpdb)
+    

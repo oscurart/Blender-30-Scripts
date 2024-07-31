4a6bce8df3725e588771233ed2aa075763307364
oscurart <info@oscurart.com.ar>
2024-07-31 19:17:56 -0300

    fix

--- oscurart_mirror_vg.py
+++ oscurart_mirror_vg.py
@@ -1,4 +1,5 @@
-#mirror vg from rigs _R to _L
+#mirror vg one side to another
+#select in edit mode
 
 import bpy
 import bmesh
@@ -11,26 +12,31 @@ me = obj.data
 #creates sym map
 bpy.ops.object.mode_set(mode="EDIT")
 bm = bmesh.from_edit_mesh(me)
-vertices = [vert.index for vert in bm.verts]
+vertices = [vert.index for vert in bm.verts if vert.select]
+vg = bpy.context.object.vertex_groups.active
 mirDict = {} 
-for group in obj.vertex_groups:
-    if group.name.count("_R"):
-        for index in vertices:            
-            bpy.ops.object.mode_set(mode="EDIT")
-            bpy.ops.mesh.select_all(action="DESELECT")  
-            bpy.ops.object.mode_set(mode="OBJECT")  
-            me.vertices[index].select = True
-            bpy.ops.object.mode_set(mode="EDIT")
-            bpy.ops.mesh.select_mirror()
-            bpy.ops.object.mode_set(mode="OBJECT") 
-            mirrorVert = [mirrorvert.index for mirrorvert in me.vertices if mirrorvert.select][0]
-            try:
-                i = group.weight(index)
-            except:
-                i = 0            
-            mirDict[mirrorVert] = [i, group.name.replace("_R","_L")]
-            #obj.vertex_groups[group.name.replace("_R","_L")].add([mirrorVert],i,"REPLACE")
+
+for index in vertices:            
+    bpy.ops.object.mode_set(mode="EDIT")
+    bpy.ops.mesh.select_all(action="DESELECT")  
+    bpy.ops.object.mode_set(mode="OBJECT")  
+    me.vertices[index].select = True
+    bpy.ops.object.mode_set(mode="EDIT")
+    bpy.ops.mesh.select_mirror()
+    bpy.ops.object.mode_set(mode="OBJECT") 
+  
+    mirrorVert = [mirrorvert.index for mirrorvert in me.vertices if mirrorvert.select][0]
+    try:
+        i = vg.weight(index)
+    except:
+        i = 0     
+               
+    mirDict[mirrorVert] = i
 
 
 for index,value in mirDict.items():
-    obj.vertex_groups[value[1]].add([index], value[0],"REPLACE")
+    print(index)
+    print(value)
+    vg.add([index], value,"REPLACE")
+
+bpy.ops.object.mode_set(mode="WEIGHT_PAINT") 
